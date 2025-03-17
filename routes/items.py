1473d902467ac5401d1947ed723f56b4850990c6
nadiaonyeje <109767752+nadiaonyeje@users.noreply.github.com>
2025-03-17 02:50:25 +0000

    Update items.py

--- routes/items.py
+++ routes/items.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, HTTPException
 from database import items_collection
+from models import WardrobeItemSchema
 
 router = APIRouter()
 
 @router.post("/items/")
-async def add_item(name: str, image_url: str, price: str, link: str):
-    new_item = {"name": name, "image_url": image_url, "price": price, "link": link}
-    result = await items_collection.insert_one(new_item)
+async def add_item(item: WardrobeItemSchema):
+    result = await items_collection.insert_one(item.dict())
     return {"message": "Item added", "id": str(result.inserted_id)}
 
 @router.get("/items/")

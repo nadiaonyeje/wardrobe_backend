c1512d5bfd86f8276947eeca8222ae953217cf3f
nadiaonyeje <109767752+nadiaonyeje@users.noreply.github.com>
2025-03-20 11:08:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 from dotenv import load_dotenv
 from starlette.middleware.sessions import SessionMiddleware
+import uvicorn
 
 
 # Load environment variables
 load_dotenv()
 
 app = FastAPI()
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Use PORT from env, default to 8000
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 app.add_middleware(SessionMiddleware, secret_key="your_secret_key")
 

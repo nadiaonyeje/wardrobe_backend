a3ab5c7485bd1a6237989a53ba3110968d9e0817
nadiaonyeje <109767752+nadiaonyeje@users.noreply.github.com>
2025-03-20 10:42:08 +0000

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta if expires_delta else timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt

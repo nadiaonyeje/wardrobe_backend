b70b8b487cb990396eae8f203f93eb704b244891
nadiaonyeje <109767752+nadiaonyeje@users.noreply.github.com>
2025-03-22 03:30:18 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,24 +1,26 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 from dotenv import load_dotenv
-from database import items_collection
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("MONGO_URI")  # Make sure this is set in your .env file
+# MongoDB connection
+DATABASE_URL = os.getenv("MONGO_URI")  # Make sure MONGO_URI is set in your .env
 
 client = AsyncIOMotorClient(DATABASE_URL)
-db = client.wardrobe_db  # Change "wardrobe_db" to whatever your database name is
+db = client.wardrobe_db  # Change this if your DB has a different name
 
 # Collections
 users_collection = db["users"]
 items_collection = db["wardrobe_items"]
 categories_collection = db["categories"]
-subcategories_collection = db["subcategories"]  
+subcategories_collection = db["subcategories"]
 outfits_collection = db["outfits"]
 
-# Create a compound index on (users_id, source) to enforce uniqueness
-items_collection.create_index(
-    [("users_id", 1), ("source", 1)],
-    unique=True
-)
+# Compound index for preventing duplicate item saves
+# This will only run if the index doesn't already exist
+async def create_indexes():
+    await items_collection.create_index(
+        [("users_id", 1), ("source", 1)],
+        unique=True
+    )

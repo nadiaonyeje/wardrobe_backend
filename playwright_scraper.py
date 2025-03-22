9ff897c32769161a70ef63f83ca45881ac749c8f
nadiaonyeje <109767752+nadiaonyeje@users.noreply.github.com>
2025-03-22 12:32:36 +0000

    Update playwright_scraper.py

--- playwright_scraper.py
+++ playwright_scraper.py
@@ -1,12 +1,9 @@
 # utils/playwright_scraper.py
 from playwright.async_api import async_playwright
 
-async with async_playwright() as p:
-    browser = await p.chromium.launch(headless=True)  # always headless in cloud
-
 async def fetch_rendered_html(url):
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True)
+        browser = await p.chromium.launch(headless=True, args=["--no-sandbox"])
         page = await browser.new_page()
         await page.goto(url, timeout=15000)
         content = await page.content()

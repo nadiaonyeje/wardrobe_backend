111c0c5d406c23f4b041b5e03ba894272f49548e
nadiaonyeje <109767752+nadiaonyeje@users.noreply.github.com>
2025-03-24 04:04:03 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ pip install -r requirements.txt
 
 # Install Playwright and Chromium in Python environment
 export PLAYWRIGHT_BROWSERS_PATH=0
-npx playwright install chromium
+playwright install chromium

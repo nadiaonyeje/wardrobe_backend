b3f7e1c83246f49ea92c691beb743cdf253c6963
nadiaonyeje <109767752+nadiaonyeje@users.noreply.github.com>
2025-03-24 03:06:22 +0000

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pip install --upgrade pip
-pip install -r requirements.txt
-
-# Tell Playwright to install in local folder (no root required)
-export PLAYWRIGHT_BROWSERS_PATH=0
-playwright install chromium
